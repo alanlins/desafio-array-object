270ccd87c27bab0ae0c0d91f74fc34246622726b
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 10:44:30 -0300

    corrigindo o erro ex02

--- Facil/ex02.js
+++ Facil/ex02.js
@@ -1,4 +1,7 @@
-//3 – Utilizando o array de , exiba o índice do item 'Cenoura'.
+//2 - Crie um array de vegetais com os seguintes valores: 'Repolho', 'Nabo', 'Rabanete', 'Cenoura' e
+//exiba o primeiro e último elemento desse array, e também o seu tamanho.
+
 let vegetais = ["Repolho", "Nabo", "Rabanete", "Cenoura"];
-let indice = vegetais.indexOf("Cenoura");
-console.log("O indice de Cenoura é: ", indice);
+console.log("Primeiro elemento do Array:", vegetais[0]);
+console.log("Ultimo elemento do Array:", vegetais[3]);
+console.log("Tamanho do Array Vegetais: " + vegetais.length);
