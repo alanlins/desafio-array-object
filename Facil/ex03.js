413dabda06550e1b384413a94185cce7e0425729
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 10:38:02 -0300

    Corrigindo Ex03- Pasta Facil

---
+++ Facil/ex03.js
@@ -0,0 +1,5 @@
+//3 – Utilizando o array de vegetais, exiba o índice do item 'Cenoura'.
+
+let vegetais = ["Repolho", "Nabo", "Rabanete", "Cenoura"];
+let indice = vegetais.indexOf("Cenoura");
+console.log("O indice de Cenoura é: ", indice);
