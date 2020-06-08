d5f565455f0f15d10446c78c0f203645a6e5f350
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 15:14:31 -0300

    Pasta Facil, Ex05

---
+++ Facil/ex05.js
@@ -0,0 +1,25 @@
+// 5 – Crie uma função que receba 4 parâmetros de entrada, e retorne um objeto com esses parâmetros como atributos.
+// (Firstname - String,
+// lastname - String,
+//  email - String,
+//  tags – Array de String)
+
+function ex05(firstname, lastname, email, tags) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.email = email;
+  this.tags = tags;
+}
+var retorne = new ex05({
+  firstname: "Augusto",
+  lastname: "Oliveira",
+  email: "augusto.oliveira_22@hotmail.com",
+  tags: [{ tags: "ex05" }],
+});
+console.log(
+  "Obj retornado: ",
+  retorne.firstname,
+  retorne.lastname,
+  retorne.email,
+  retorne.tags
+);
