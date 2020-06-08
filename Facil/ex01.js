2f4d9f3504b64f985ba1e29b9cf804afe68b48ba
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 10:57:35 -0300

    Corrgindo erros , Ex01 - Pasta Facil

--- Facil/ex01.js
+++ Facil/ex01.js
@@ -1,7 +1,4 @@
-// let numbers = [1, 2, 3, 4, 5];
-// numbers.push(6, 7, 8, 9);
-// console.log("Ex01: ", numbers);
 //1-
-let numbers = [1, 2, 3, 4, 5];
+let numbers = [0, 1, 2, 3, 4, 5];
 numbers.push(6, 7, 8, 9);
 console.log("Ex01: ", numbers);
--- Facil/ex02.js
+++
@@ -1,4 +0,0 @@
-//3 – Utilizando o array de , exiba o índice do item 'Cenoura'.
-let vegetais = ["Repolho", "Nabo", "Rabanete", "Cenoura"];
-let indice = vegetais.indexOf("Cenoura");
-console.log("O indice de Cenoura é: ", indice);
