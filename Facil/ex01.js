37fb31a2a197d8010f7793d57ebfbfd763928991
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 10:16:13 -0300

    Ex-01 Pasta FACIL

--- Facil/ex01.js
+++ Facil/ex01.js
@@ -1,3 +1,7 @@
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.push(6, 7, 8, 9);
+// console.log("Ex01: ", numbers);
+//1-
 let numbers = [1, 2, 3, 4, 5];
 numbers.push(6, 7, 8, 9);
 console.log("Ex01: ", numbers);
