e6bc246c3223b4545311ee561a5f447d872ca104
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 16:55:57 -0300

    Corrigindo Erro, Ex02

--- Facil/ex02.js
+++ Facil/ex02.js
@@ -2,7 +2,7 @@
 //exiba o primeiro e último elemento desse array, e também o seu tamanho.
 
 let vegetais = ["Repolho", "Nabo", "Rabanete", "Cenoura"];
-let ultimaPosicao = vegetais.pop();
+let ultimaPosicao = vegetais[vegetais.length - 1];
 console.log("Primeiro elemento do Array:", vegetais[0]);
 console.log("Ultimo elemento do Array:", ultimaPosicao);
 console.log("Tamanho do Array Vegetais: " + vegetais.length);
