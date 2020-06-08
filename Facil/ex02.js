cd2e425647b2d39ae81303e5ab64d2650ddd1704
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 10:18:51 -0300

    Ex02 - Pasta Facil

---
+++ Facil/ex02.js
@@ -0,0 +1,4 @@
+//3 – Utilizando o array de , exiba o índice do item 'Cenoura'.
+let vegetais = ["Repolho", "Nabo", "Rabanete", "Cenoura"];
+let indice = vegetais.indexOf("Cenoura");
+console.log("O indice de Cenoura é: ", indice);
