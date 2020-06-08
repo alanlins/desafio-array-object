dedf31a0f73c45888e33aa5246c7f4d792c1f436
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-08 11:23:47 -0300

    Corrigindo o Ajuste do pull Request

--- Facil/ex02.js
+++ Facil/ex02.js
@@ -2,6 +2,7 @@
 //exiba o primeiro e último elemento desse array, e também o seu tamanho.
 
 let vegetais = ["Repolho", "Nabo", "Rabanete", "Cenoura"];
+let ultimaPosicao = vegetais.pop();
 console.log("Primeiro elemento do Array:", vegetais[0]);
-console.log("Ultimo elemento do Array:", vegetais[3]);
+console.log("Ultimo elemento do Array:", ultimaPosicao);
 console.log("Tamanho do Array Vegetais: " + vegetais.length);
