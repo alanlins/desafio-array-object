bf8111e4d44f14d050e0d921b62952bc27f1877e
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-09 11:01:37 -0300

    Pasta  media Ex09, e ajustando pasta Media

---
+++ Medio/ex09.js
@@ -0,0 +1,5 @@
+//9 – Com o array de numbers, utilize o método map para criar um novo array com o dobro de valor de cada item
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const result = numbers.map((n) => n * 2);
+console.log(result);
