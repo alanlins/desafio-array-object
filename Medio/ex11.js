cd05dc444eb0a69a99ef5fbd5534455334e09aae
Augusto Sergio Araujo de Oliveira <augusto.oliveira@pampa.compasso>
2020-06-12 15:57:26 -0300

    correcao ex11

--- Medio/ex11.js
+++ Medio/ex11.js
@@ -2,22 +2,12 @@
 //mas dessa vez utilize o método toString() dos objetos,
 //criando sua própria implementação dele. Pode usar console.log para imprimir na tela.
 
-function ex05(firstname, lastname, email, tags) {
-  return {
-    firstname: firstname,
-    lastname: lastname,
-    email: email,
-    tags: tags,
-  };
-}
-
-function exibir() {
-  return ex05("Augusto", "Oliveira ", "augusto.oliveira_22@hotmmail.com", [
-    { tags: "Primeiro Codigo" },
-  ]);
-}
-const toResultOne = Object.keys(exibir());
-const toResultTwo = Object.values(exibir());
-
-console.log(toResultOne.toString());
-console.log(toResultTwo.toString());
+const user = {
+  name: "Augusto",
+  sobrenome: "Oliveira",
+  email: "Augusto@Hotmail.com",
+};
+const toEmail = user.email.toString();
+const toResult = user.name.toString();
+const toResolve = user.sobrenome.toString();
+console.log("name:", toResult, "/", "sobrenome:", toResolve, "email:", toEmail);
